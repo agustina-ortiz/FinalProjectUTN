5179b4ed841b306c395e169ab9ce018ce623f47a
María Agustina Ortiz <95661814+agustina-ortiz@users.noreply.github.com>
2023-09-04 19:42:40 -0300

    final version

--- final-project-utn/src/components/NavBar/index.js
+++ final-project-utn/src/components/NavBar/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import './style.css';
+import { BiSolidPhoneCall, BiMenu } from 'react-icons/bi'
 
 const NavBar = () => {
     return (
-        <nav className="navbar">
+        <div className="navbar">
             <div className="title">
                 <p className="titleText">Aamir & Hameeda</p>
             </div>
-            <ul className="list">
-                <li><a className="active" href="#home">Home</a></li>
-                <li><a href="#news">About</a></li>
-                <li><a href="#contact">Portfolio</a></li>
-                <li><a href="#about">Awards</a></li>
-                <li><a href="#about">Contact</a></li>
-            </ul>
-            <p className="telephone">+91 40 6678 4993</p>
-        </nav>
+            <input type="checkbox" id="check" />
+            <label for="check" className="menuIcon">
+                <i><BiMenu /></i>
+            </label>
+            <nav className="nav">
+                <ul className="list">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#awards">Awards</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+            <div className="phoneContainer">
+                <span className="phoneIcon"><BiSolidPhoneCall /></span>
+                <p className="telephone">+91 40 6678 4993</p>
+            </div>
+        </div>
     )
 }
 
